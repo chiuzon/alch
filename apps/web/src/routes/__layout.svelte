bb3aa02a95ecbdeb4eb0f92a48ba180d66ab34c0
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-07-10 18:16:13 +0300

    Navbar

--- apps/web/src/routes/__layout.svelte
+++ apps/web/src/routes/__layout.svelte
@@ -2,10 +2,16 @@
 	import '$lib/styles/global.css';
 </script>
 
-<div class=" bg-orange-300 shadow-sm py-3 flex justify-center">
-	<div class="w-1/2 inline-flex justify-between items-center">
-		<div class=" text-orange-600 font-bold text-xl">ALCH</div>
-		<div>NAVBAR</div>
+<div class="flex border-b justify-center">
+	<div class="w-1/2 inline-flex flex-col md:flex-row justify-between items-center">
+		<div class="bg-white text-secondary-1 font-bold text-xl py-3 px-5 font-titan-one">ALCH</div>
+		<div class="flex-1 py-2 flex justify-end items-center gap-2">
+			<a href="/" class=" hover:bg-white hover:text-secondary-1 px-2 py-1">Home</a>
+			<a href="" class="hover:bg-white hover:text-secondary-1 px-2 py-1">Trade</a>
+			<button class="bg-white text-secondary-1  hover:bg-opacity-50 active:bg-opacity-95 px-2 py-1"
+				>Connect</button
+			>
+		</div>
 	</div>
 </div>
 
--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				secondary: {
+					1: '#46c4ea',
+					2: '#68ceee',
+					3: '#01a0ca'
+				}
+			},
+			fontFamily: {
+				'titan-one': ['Titan One', 'cursive'],
+				poppins: ['Poppins', ' sans-serif']
+			}
+		}
 	},
 	plugins: []
 };
