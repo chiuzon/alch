27a819f1456a0d0bede62b22c77caf3cf98a0dae
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-07-10 17:53:15 +0300

    Styling

--- apps/web/src/routes/__layout.svelte
+++ apps/web/src/routes/__layout.svelte
@@ -2,4 +2,11 @@
 	import '$lib/styles/global.css';
 </script>
 
+<div class=" bg-orange-300 shadow-sm py-3 flex justify-center">
+	<div class="w-1/2 inline-flex justify-between items-center">
+		<div class=" text-orange-600 font-bold text-xl">ALCH</div>
+		<div>NAVBAR</div>
+	</div>
+</div>
+
 <slot />
